4f50a57857c931879ba6b9fc171ba62203c9153e
kimeuijin <kej1480k@naver.com>
2022-07-12 17:50:41 +0900

    move-script add

--- portfolio-site/js/mobile-button.js
+++ portfolio-site/js/mobile-button.js
@@ -1,16 +1,19 @@
-const hamberger = document.getElementById("menu");
-const menuBar = document.querySelector(".gnb");
 
-let subToggle = true;
-function showMenu(){
-    if(subToggle) {
-        menuBar.style.setProperty('height', 'calc(100vh - 80px)');
-        subToggle =! subToggle;
-      } else {
-        menuBar.style.height = "0px";
-        subToggle =! subToggle;
+// document.querySelector(".close-btn").addEventListener("click", showMenu);
+window.onload = function() {
+    const hamberger = document.querySelector(".hamburgerButton");
+    const menuBar = document.querySelector(".gnb");
+    
+    let subToggle = true;
+    function showMenu(){
+        if(subToggle) {
+            menuBar.style.setProperty('height', 'calc(100vh - 80px)');
+            subToggle =! subToggle;
+        } else {
+            menuBar.style.height = "0px";
+            subToggle =! subToggle;
+        }
     }
+    
+    hamberger.addEventListener("click", showMenu);
 }
-
-hamberger.addEventListener("click", showMenu);
-document.querySelector(".close-btn").addEventListener("click", showMenu);
