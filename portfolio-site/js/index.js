88a38b668a1bb3221917547d4711cbaaf4412f18
kimeuijin <kej1480k@naver.com>
2022-11-15 02:36:12 +0900

    hamburger button

--- portfolio-site/js/index.js
+++ portfolio-site/js/index.js
@@ -1,59 +1,62 @@
-//toggle button
-$(function() {
-    // $(".menu-box").hide();
-    $(".hamburger").click(function() {
+$(function() { //toggle button
+    $(".wrap").click(function() {
         $(".menu-box").slideToggle();
     });
 
+    $(".wrap").click(function() {
+        $(".toggle-button").toggleClass(".button-open");
+    });
+
+
     //graph
-    $(window).ready(function(){
+    $(window).ready(function() {
         draw(95, '.graph-h', '#ff8363');
         draw(95, '.graph-c', '#009de6');
-        draw(95, '.graph-s','#cf649a');
-        draw(80, '.graph-j','#ffca3e');
-        draw(80, '.graph-r','#61dafb');
-        draw(75, '.graph-jq','#535556');
-        draw(95, '.graph-d','#0a1f35');
-        
+        draw(95, '.graph-s', '#cf649a');
+        draw(80, '.graph-j', '#ffca3e');
+        draw(80, '.graph-r', '#61dafb');
+        draw(75, '.graph-jq', '#535556');
+        draw(95, '.graph-d', '#0a1f35');
+
     });
-    
-    function draw(max, classname, colorname){
+
+    function draw(max, classname, colorname) {
         var i = 1;
-        var func1 = setInterval(function(){
-            if(i<max){
-                color1(i,classname,colorname);
+        var func1 = setInterval(function() {
+            if (i < max) {
+                color1(i, classname, colorname);
                 i++;
-            } else{
+            } else {
                 clearInterval(func1);
             }
-        },10);
+        }, 10);
     }
-    
-    function color1(i, classname,colorname){
+
+    function color1(i, classname, colorname) {
         $(classname).css({
-            "background":"conic-gradient("+colorname+" 0% "+i+"%, #f7f0e0 "+i+"% 100%)"
+            "background": "conic-gradient(" + colorname + " 0% " + i + "%, #f7f0e0 " + i + "% 100%)"
         });
     }
 
 
     // skill view
-    var smenu = document.getElementsByClassName('skill-menu'); 
-    var graph = document.getElementsByClassName('graph');  
-    
-    graph[0].style.display = "block";  
-    
-    let smenulength = smenu.length; 
+    var smenu = document.getElementsByClassName('skill-menu');
+    var graph = document.getElementsByClassName('graph');
+
+    graph[0].style.display = "block";
+
+    let smenulength = smenu.length;
     for (let i = 0; i < smenulength; i++) {
-        smenu[i].addEventListener("click", function() { 
+        smenu[i].addEventListener("click", function() {
             skillView(i);
         });
     }
 
     // determine skil text display
     function skillView(num) {
-        let graphlength = graph.length; 
+        let graphlength = graph.length;
         for (let i = 0; i < graphlength; i++) {
-            let mygraph = graph[i]; 
+            let mygraph = graph[i];
             if (i == num) {
                 mygraph.style.display = "block";
             } else {
@@ -63,7 +66,7 @@ $(function() {
     }
 
 
-    
+
 });
 
 
