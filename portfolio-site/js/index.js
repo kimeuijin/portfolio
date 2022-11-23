368ea140577a1711d035bd53b7417361d73dd391
kimeuijin <kej1480k@naver.com>
2022-11-23 17:44:47 +0900

    design page 완

--- portfolio-site/js/index.js
+++ portfolio-site/js/index.js
@@ -102,26 +102,3 @@ $(function() { //toggle button
     }
 
 });
-
-
-
-//modal 
-// const overlay = document.querySelector('#overlay');
-// const photoCon = document.querySelector('.photo');
-// const colseButton = document.querySelector('.close');
-
-// function showBic(e) { //e는 이벤트가 발생된 대상
-//     const orgImg = e.querySelector("img").src;
-//     const output = `<img src="${orgImg}"><p>${e.querySelector("p").innerHTML} </p>`;
-
-//     photoCon.innerHTML = output;
-//     overlay.classList.add("show");
-
-//     document.body.classList.add("stop-scroll"); 
-// }
-
-
-// colseButton.addEventListener('click', () => {
-//     overlay.classList.remove("show");
-//     document.body.classList.remove("stop-scroll");
-// })
